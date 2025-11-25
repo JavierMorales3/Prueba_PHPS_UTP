d325d9a1355d6be107c6c931b44f9ec538542ac8
JavierMorales3 <142769105+JavierMorales3@users.noreply.github.com>
2025-11-24 20:13:19 -0500

    Delete PROYECTO directory

--- PROYECTO/database/migrations/003_create_user_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE Usuarios (
-    usuario_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    nombre VARCHAR(100) NOT NULL,
-    apellido VARCHAR(100) NOT NULL,
-    correo_electronico VARCHAR(100) UNIQUE NOT NULL,
-    rol VARCHAR(100)
-);
